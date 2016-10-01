ebbd9d91a8ddc4e943d265072808563a67100e30
Nathanael See <nathanaelsee@yahoo.com>
2016-10-01 06:13:53 -0700

    prettyfied layout

--- app/src/main/java/com/example/cathy/sdapp/SecondActivity.java
+++ app/src/main/java/com/example/cathy/sdapp/SecondActivity.java
@@ -15,57 +15,57 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        Button button1 = (Button) findViewById(R.id.button1);
+        Button button1 = (Button) findViewById(R.id.menu_cat1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToCategoryXActivity(1);
             }
         });
-        Button button2 = (Button) findViewById(R.id.button2);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Button button2 = (Button) findViewById(R.id.menu_cat2);
+        button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToCategoryXActivity(2);
             }
         });
-        Button button3 = (Button) findViewById(R.id.button3);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Button button3 = (Button) findViewById(R.id.menu_cat3);
+        button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToCategoryXActivity(3);
             }
         });
-        Button button4 = (Button) findViewById(R.id.button4);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Button button4 = (Button) findViewById(R.id.menu_cat4);
+        button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToCategoryXActivity(4);
             }
         });
-        Button button5 = (Button) findViewById(R.id.button5);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Button button5 = (Button) findViewById(R.id.menu_cat5);
+        button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToCategoryXActivity(5);
             }
         });
-        Button button6 = (Button) findViewById(R.id.button6);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Button button6 = (Button) findViewById(R.id.menu_cat6);
+        button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToCategoryXActivity(6);
             }
         });
-        Button button7 = (Button) findViewById(R.id.button7);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Button button7 = (Button) findViewById(R.id.menu_cat7);
+        button7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToCategoryXActivity(7);
             }
         });
-        Button button8 = (Button) findViewById(R.id.button8);
-        button1.setOnClickListener(new View.OnClickListener() {
+        Button button8 = (Button) findViewById(R.id.menu_cat8);
+        button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToCategoryXActivity(8);
@@ -74,38 +74,8 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     private void goToCategoryXActivity(int x) {
-        if (x == 1) {
-            Intent int1 = new Intent(this, CategoryOneActivity.class);
-            startActivity(int1);
-        }
-        if (x == 2) {
-            Intent int2 = new Intent(this, CategoryTwoActivity.class);
-            startActivity(int2);
-        }
-        if (x == 3) {
-            Intent int3 = new Intent(this, CategoryThreeActivity.class);
-            startActivity(int3);
-        }
-        if (x == 4) {
-            Intent int4 = new Intent(this, CategoryFourActivity.class);
-            startActivity(int4);
-        }
-        if (x == 5) {
-            Intent int5 = new Intent(this, CategoryFiveActivity.class);
-            startActivity(int5);
-        }
-        if (x == 6) {
-            Intent int6 = new Intent(this, CategorySixActivity.class);
-            startActivity(int6);
-        }
-        if (x == 7) {
-            Intent int7 = new Intent(this, CategorySevenActivity.class);
-            startActivity(int7);
-        }
-        if (x == 8) {
-            Intent int8 = new Intent(this, CategoryEightActivity.class);
-            startActivity(int8);
-        }
-
+        Intent quizIntent = new Intent(this, CategoryEightActivity.class);
+        quizIntent.putExtra("Category", x);
+        startActivity(quizIntent);
     }
 }
