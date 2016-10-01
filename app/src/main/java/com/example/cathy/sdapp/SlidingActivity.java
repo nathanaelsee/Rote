4a21bbb10763ca46a5e9df50aeb3edaff8c6eeb6
Joshua Zeitsoff <jzeitsoff@berkely.edu>
2016-10-01 09:01:47 -0700

    Added sliding?

---
+++ app/src/main/java/com/example/cathy/sdapp/SlidingActivity.java
@@ -0,0 +1,25 @@
+package com.example.cathy.sdapp;
+
+/**
+ * Created by joshuazeitsoff on 10/1/16.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+public class SlidingActivity extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.sliding, container, false);
+
+        return rootView;
+    }
+}
+
+
+
+
