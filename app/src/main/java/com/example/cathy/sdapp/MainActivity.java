665138915da024aa92cd3af18f36013487d20113
Cathy Pham-Le <cathyphamle@berkeley.edu>
2016-10-01 03:37:25 -0700

    based off tutorial

--- app/src/main/java/com/example/cathy/sdapp/MainActivity.java
+++ app/src/main/java/com/example/cathy/sdapp/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.cathy.sdapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
+        });
     }
+
+    private void goToSecondActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
